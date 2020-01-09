c318942d6f99f31eb1c796df7ce917bcf4ad26be
avizmarques <aviz.marques@gmail.com>
2020-01-09 16:27:56 +0100

    create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,20 @@
-function submitContent() {
-    console.log('Hello!');
+function submitComment() {
+    // gather data
+    const name = document.querySelector('#name').value;
+    const msg = document.querySelector('#msg').value;
+
+    // create elements necessary
+    const comment = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
+    
+    // adjust elements
+    h3.innerHTML = `${name} said`;
+    p.innerHTML = msg
+    comment.classList.add('comment');
+    comment.appendChild(h3);
+    comment.appendChild(p);
+    console.log(comment);
+
+
 }
